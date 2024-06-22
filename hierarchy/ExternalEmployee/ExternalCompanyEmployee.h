3a184514aa6a8a325b05f322c1b000c80400f158
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-22 10:46:44 +0300

    Update ExternalCompanyEmployee.h
    create an initial outline

--- hierarchy/ExternalEmployee/ExternalCompanyEmployee.h
+++ hierarchy/ExternalEmployee/ExternalCompanyEmployee.h
@@ -2,18 +2,39 @@
 #include "IExternalCompanyEmployee.h"
 #include "User.h"
 #include "Pair.h"
-
-
+#include <random>
+#include <cstring>
 
 struct BankCheck{
-    Pair<char[3], double> check;
+    char verificationCode[3];
+    double sum;
+
+    BankCheck(const char* code, double sum) {
+        strcpy(verificationCode, code);
+
+        this->sum = sum;
+    }
 };
 
 
 
 class ExternalCompanyEmployee : public User, public virtual IExternalEmployee{
 public :
-    void send_check (double sum, const MyString& bankName, const MyString& EGN) override;
-private:
+    ExternalCompanyEmployee(const MyString& firstName, const MyString& lastName,
+                            const MyString& EGN, int age, const MyString& password);
+    ExternalCompanyEmployee(MyString&& firstName, MyString&& lastName,
+                            MyString& EGN, int age, MyString&& password);
 
+    ExternalCompanyEmployee() = default;
+
+
+    void send_check (double sum, const MyString& bankName, const MyString& EGN) override;
+    void send_check (double sum, MyString&& bankName, MyString&& EGN) override;
+
+protected:
+    void saveToFile(std::ofstream ofs) const override;
+    void readFromFile(std::ifstream& ifs) override;
+private:
+    const char* generateVerificationCode ();
 };
+

a06896a7275260802c6d7cbac83e678a2b0453b3
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-22 10:43:19 +0300

    Update User.h

--- hierarchy/User/User.h
+++ hierarchy/User/User.h
@@ -5,21 +5,37 @@
 class User : public virtual IUser  {
 public:
 
-    User (const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age, const MyString& password );
+    User (const MyString& firstName, const MyString& surname, const MyString& EGN,
+          unsigned age, const MyString& password );
+    User(MyString&& firstName, MyString&& lastName, MyString&& EGN,
+         unsigned age, MyString&& password);
+
+    User() = default;
+
     void whoAmI() const override;
-    void exit() override;
+
+    virtual void saveToFile(std::ofstream& ofs) const = 0;
+    virtual void readFromFile(std::ifstream& ifs) = 0 ;
+
+    virtual ~User() = default;
+//    void exit() override;
 
     //not overriding the help function
-private:
+
+protected:
+    static int currId;
+
+    int id = 0;
+
     MyString firstName;
     MyString surname;
     MyString password;
 
     unsigned EGN[10]; //will be string in the constructor
 
-    unsigned age;
+    unsigned age = 1;
 
-    void setEGN ();
-
+    void setEGN (const MyString& EGN);
+    void setPassword(const MyString& password);
 
 };

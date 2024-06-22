cb38a8d6d32016bcfcd8642b4ef8593fa887c731
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-22 20:10:35 +0300

    Update BankEmployee.h
    provide updated BankEmployee logic

--- hierarchy/BankEmployee/BankEmployee.h
+++ hierarchy/BankEmployee/BankEmployee.h
@@ -1,44 +1,20 @@
 #pragma once
-#include "MyString.h"
 #include "IBankEmployee.h"
-#include "User.h"
-
-#include "Vector.h"
-#include "Client.h"
-
-enum class TaskType {
-    OPEN,
-    CLOSE,
-    CHANGE,
-
-    Default
-};
-
-struct Task {
-    TaskType type = TaskType::Default;
-    const Client* client = nullptr;
-    MyString details;
-    unsigned taskId = 0;
-
-    Task () = default;
-    Task (const TaskType& t, const Client* c, const MyString& d, unsigned id ) :
-                                  type(t), client(c), details(d), taskId(id) {}
-    ~Task() {
-        delete client;
-    }
-};
+#include "Task.h"
 
 class BankEmployee : public User, public virtual IBankEmployee {
 public:
-    BankEmployee( const MyString& firstName, const MyString& surname, const MyString& EGN,
-                  unsigned age, const MyString& password) :
-                  User(firstName,surname,EGN,age,password) {}
+    BankEmployee(const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age, const MyString& password);
+
+    void show_tasks() const override;
+    void view(const MyString& taskId) const override;
+    void approve(const MyString& taskId) override;
+    void disapprove(const MyString& taskId, const MyString& message) override;
+    void validate(const MyString& taskId) override;
+
+    void addTask(const Task& task);
+    size_t getTaskCount() const;
+
 private:
-
-    //сравняваме спрямо това чий сайз е най-малкия -> той изпълнява поръчката
     Vector<Task> tasks;
-    size_t taskCounter = 0;
-
 };
-
-

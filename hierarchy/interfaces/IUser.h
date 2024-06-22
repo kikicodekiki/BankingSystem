dc712f15e13df3ab89bf2ecfaaeee7ce71f8539b
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-22 10:45:32 +0300

    Update IUser.h
    update to the initial logic - add functions for file management

--- hierarchy/interfaces/IUser.h
+++ hierarchy/interfaces/IUser.h
@@ -1,10 +1,13 @@
 #pragma once
-
+#include <fstream>
 class IUser {
 public:
-    virtual void exit() = 0;
+//    virtual void exit() = 0;
     virtual void whoAmI() const = 0;
     virtual void help() const = 0;
+
+    virtual void saveToFile(std::ofstream ofs) const = 0;
+    virtual void readFromFile(std::ifstream& ifs) = 0;
 
     virtual ~IUser() = default;
 };

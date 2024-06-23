b3c108d686573dda4d0afdd3297c583ec04cf211
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 20:15:19 +0300

    Create Task.h

---
+++ SystemManagement/ManagementForBank/TaskManagement/Task.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "User.h"
+#include "MyString.h"
+#include "Vector.h"
+#include "TaskState.h"
+#include "Client.h"
+#include "Bank.h"
+
+enum class TaskType {
+    OPEN,
+    CLOSE,
+    CHANGE,
+    Default
+};
+
+class Task {
+public:
+    TaskType type = TaskType::Default;
+    const Client* client = nullptr;
+    const Bank* bank = nullptr;
+    unsigned taskId = 0;
+    TaskState* state = nullptr;
+    MyString accountNumber;
+    MyString details;
+
+    Task() = default;
+    Task(const TaskType& t, const Client* c, const MyString& accNum, const MyString& d, unsigned id);
+    ~Task();
+
+    void approve();
+    void reject(const MyString& message);
+    void setState(TaskState* newState);
+
+private:
+    void cleanupState();
+};

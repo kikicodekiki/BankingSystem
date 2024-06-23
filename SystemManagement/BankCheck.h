7099611222eb30d2750008aad8ccb8c6ec08d36b
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 14:48:07 +0300

    Create BankCheck.h

---
+++ SystemManagement/BankCheck.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <cstring>
+#include <random>
+
+
+
+struct BankCheck {
+    char verificationCode[3];
+    double sum;
+
+    BankCheck(const char* code, double sum) {
+        strcpy(verificationCode, code);
+        this->sum = sum;
+    }
+
+    ~BankCheck() = default;
+
+    const char* getVerificationCode () const {
+        return  verificationCode;
+    }
+
+    double getSum () const {
+        return sum;
+    }
+
+    const char* generateVerificationCode() {
+        static int codeSize = 3;
+
+        static const char symbolsToUse[] =
+                "0123456789"
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                "abcdefghijklmnopqrstuvwxyz";
+        char code[codeSize + 1];
+
+        for (int i = 0; i < codeSize; i++) {
+            code[i] = symbolsToUse[rand() % (sizeof(symbolsToUse) - 1)];
+        }
+
+        code [codeSize] = '\0';
+        return code;
+    }
+
+    void setCode () {
+        strcpy(verificationCode, generateVerificationCode());
+    }
+
+    void setCode(const char* code) {
+        strcpy(verificationCode, code);
+    }
+
+
+    double setSum (double sum) {
+        if(sum > 0) {
+            this->sum = sum;
+        }
+    }
+};

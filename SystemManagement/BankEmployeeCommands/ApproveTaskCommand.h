e168ad3926fce87459337860e08b0f0137551dcc
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 19:46:10 +0300

    Update ApproveTaskCommand.h

--- SystemManagement/BankEmployeeCommands/ApproveTaskCommand.h
+++ SystemManagement/BankEmployeeCommands/ApproveTaskCommand.h
@@ -12,6 +12,9 @@ public:
     void execute () override {
         Application::getInstance().approveTask(taskId, employeeId);
     }
+     ICommand* clone() const override {
+        return new ApproveTaskCommand(*this);
+    }
 private:
     MyString taskId;
 };

412fe2c2ce556c62bf90d0d9e5bc54929c2ef392
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 19:54:41 +0300

    Update Client.h
    add functionalities to the client class

--- hierarchy/Client/Client.h
+++ hierarchy/Client/Client.h
@@ -1,39 +1,39 @@
 #pragma once
-#include "IUser.h"
 #include "User.h"
 #include "IClient.h"
 #include "Vector.h"
 #include "MyString.h"
-#include "PolymorphicPtr.h"
+#include <fstream>
 
 class Client : public User, public virtual IClient {
 public:
-    Client (const MyString& firstName, const MyString& surname,
-            const MyString& EGN, unsigned age, const MyString& password,
-            const MyString& address);
-
+    Client(const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age,
+           const MyString& password, const MyString& address);
     Client(MyString&& firstName, MyString&& surname, MyString&& EGN, unsigned age,
            MyString&& password, MyString&& address);
 
-    void check_avl (const MyString& bankName, const MyString& accountNumber) const override;
-    void open (const MyString& bankName) override;
-    void close (const MyString& bankName, const MyString& accountNumber) = 0;
-    void redeem (const MyString& bankName, const MyString& accountNumber, const MyString& verificationCode) override;
-    void change (const MyString& newBankName, const MyString& currBank, const MyString& accNum) override;
-    void list (const MyString& bankName) const override;
-    void messages () const override;
+    void check_avl(const MyString& bankName, const MyString& accountNumber) const override;
+    void open(const MyString& bankName) override;
+    void close(const MyString& bankName, const MyString& accountNumber) override;
+    void redeem(const MyString& bankName, const MyString& accountNumber, const MyString& verificationCode) override;
+    void change(const MyString& newBankName, const MyString& currBank, const MyString& accNum) override;
+    void list(const MyString& bankName) const override;
+    void messages() const override;
 
-    void help() const override;
-
-    void addAccount (const MyString& accountName);
+    void addAccount(const MyString& accountName);
+    void addMessage(const MyString& message);
 
     void saveToFile(std::ofstream& ofs) const override;
     void readFromFile(std::ifstream& ifs) override;
 
-private:
+    void help() const override;
 
+    User* clone() const override {
+        return new Client(*this);
+    }
+    Vector<MyString> getAccounts() const;
+private:
     MyString address;
     Vector<MyString> bankAccounts;
     Vector<MyString> messageInbox;
-
 };

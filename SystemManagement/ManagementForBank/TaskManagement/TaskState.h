2a17fc0978349bdad6f6e576773f000fa7fb30bc
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 20:16:01 +0300

    Create TaskState.h

---
+++ SystemManagement/ManagementForBank/TaskManagement/TaskState.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "MyString.h"
+
+class Task;
+
+class TaskState {
+public:
+    virtual void approve(Task& task) = 0;
+    virtual void reject(Task& task, const MyString& message) = 0;
+    virtual ~TaskState() = default;
+};
+
+class Approved : public TaskState {
+public:
+    void approve(Task& task) override;
+    void reject(Task& task, const MyString& message) override;
+};
+
+class Rejected : public TaskState {
+public:
+    void approve(Task& task) override;
+    void reject(Task& task, const MyString& message) override;
+};
+
+class NeedsApproval : public TaskState {
+public:
+    void approve(Task& task) override;
+    void reject(Task& task, const MyString& message) override;
+};

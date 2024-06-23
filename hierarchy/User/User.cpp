2e04f5232e86a950a3cb8d03b231661606860c03
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 20:24:06 +0300

    Update User.cpp

--- hierarchy/User/User.cpp
+++ hierarchy/User/User.cpp
@@ -1,11 +1,15 @@
+#pragma once
 #include "User.h"
+#include "MyString.h"
 #include "Vector.h"
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <cstring>
 
 int User::currId = 1;
 
-static Vector<unsigned> parseMyStringToUnsignedArr (const MyString& str) {
+static Vector<unsigned> parseMyStringToUnsignedArr(const MyString& str) {
     Vector<unsigned> result;
 
     const char* cstr = str.c_str();
@@ -14,7 +18,9 @@ static Vector<unsigned> parseMyStringToUnsignedArr (const MyString& str) {
     while (*cstr != '\0') {
         unsigned value = std::strtoul(cstr, &end, 10);
 
-        if (cstr == end) {break;}
+        if (cstr == end) {
+            break;
+        }
 
         result.pushBack(value);
 
@@ -28,47 +34,66 @@ static Vector<unsigned> parseMyStringToUnsignedArr (const MyString& str) {
     return result;
 }
 
-void User::setEGN (const MyString& EGN) {
+void User::setEGN(const MyString& EGN) {
     Vector<unsigned> parseEGN = parseMyStringToUnsignedArr(EGN);
 
-    if (parseEGN.getSize() != 10) {
-        throw std::invalid_argument("EGN must be exactly 10 digits long");
+    if (parseEGN.getSize() != EGN_SIZE) {
+        throw std::invalid_argument("EGN must be exactly 10 digits long\n");
     }
 
-    for (size_t i = 0; i < 10; i++) {
+    for (size_t i = 0; i < EGN_SIZE; ++i) {
         this->EGN[i] = parseEGN[i];
     }
 }
 
 void User::setPassword(const MyString& password) {
-    if (password == (firstName+surname)) {
+    if (password == (name + surname)) {
         throw std::invalid_argument("The password should differ from your username");
     }
 
-    this-> password = password;
+    this->password = password;
 }
 
-
-User::User (const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age,
-            const MyString& password ) : firstName(firstName), surname(surname)  {
+User::User (const MyString& name, const MyString& surname,
+      const MyString& EGN, size_t age,
+      const MyString& password) : name(name), surname(surname), age(age){
 
     setEGN(EGN);
-
-    this -> age = age;
-
     setPassword(password);
-
     id = currId++;
-
 }
 
-User::User(MyString&& firstName, MyString&& surname, MyString&& EGN, unsigned age, MyString&& password)
-        : firstName(std::move(firstName)), surname(std::move(surname)), age(age), password(std::move(password))
-        {
-           id = currId++;
-           setEGN(std::move(EGN));
+
+User :: User (MyString&& name, MyString&& surname,
+        MyString&& EGN, size_t age,
+        MyString&& password) : name (std::move(name)), surname(std::move(surname)), age(age){
+    setEGN(std::move(EGN));
+    setPassword(std::move(password));
+    id = currId++;
 }
 
-void User ::whoAmI() const {
-    std::cout << "You are: " << firstName << " " << surname << "\n";
+void User::whoAmI() const {
+    std::cout << "Name: " << name.c_str() << " " << surname.c_str() << "\n";
+    std::cout << "EGN: ";
+    for (size_t i = 0; i < EGN_SIZE; ++i) {
+        std::cout << EGN[i];
+    }
+    std::cout << "\nAge: " << age << "\n";
+}
+
+bool User::authenticate(const MyString& username, const MyString& password) const {
+    return getFullName() == username && this->password == password;
+}
+
+MyString User::getFullName() const {
+    return name + " " + surname;
+}
+
+
+void User::saveToFile(std::ofstream& ofs) const {
+    ofs << name << '\n' << surname << '\n' << age << '\n' << password << '\n';
+    for (size_t i = 0; i < EGN_SIZE; ++i) {
+        ofs << EGN[i];
+    }
+    ofs << '\n';
 }

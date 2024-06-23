16c01999829feed474adbb7ffda9368f0aee0a77
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 20:00:50 +0300

    Update ExternalCompanyEmployee.h

--- hierarchy/ExternalEmployee/ExternalCompanyEmployee.h
+++ hierarchy/ExternalEmployee/ExternalCompanyEmployee.h
@@ -1,40 +1,32 @@
 #pragma once
-#include "IExternalCompanyEmployee.h"
 #include "User.h"
-#include "Pair.h"
+#include "IExternalEmployee.h"
+#include "MyString.h"
 #include <random>
-#include <cstring>
 
-struct BankCheck{
-    char verificationCode[3];
-    double sum;
 
-    BankCheck(const char* code, double sum) {
-        strcpy(verificationCode, code);
 
-        this->sum = sum;
+class ExternalEmployee : public User, virtual public IExternalEmployee {
+public:
+    ExternalEmployee() = default;
+    ExternalEmployee(const MyString& firstName, const MyString& lastName,
+                            const MyString& EGN, size_t age, const MyString& password);
+    ExternalEmployee(MyString&& firstName, MyString&& lastName,
+            MyString& EGN, size_t age, MyString&& password);
+
+    void send_check (double sum, const char* verificationCode, const MyString& EGN) override;
+    void send_check (double sum, char*&& verificationCode, MyString&& EGN) override;
+
+    void help() const override;
+
+    User* clone() const override {
+        return new ExternalEmployee(*this);
     }
-};
-
-
-
-class ExternalCompanyEmployee : public User, public virtual IExternalEmployee{
-public :
-    ExternalCompanyEmployee(const MyString& firstName, const MyString& lastName,
-                            const MyString& EGN, int age, const MyString& password);
-    ExternalCompanyEmployee(MyString&& firstName, MyString&& lastName,
-                            MyString& EGN, int age, MyString&& password);
-
-    ExternalCompanyEmployee() = default;
-
-
-    void send_check (double sum, const MyString& bankName, const MyString& EGN) override;
-    void send_check (double sum, MyString&& bankName, MyString&& EGN) override;
-
 protected:
-    void saveToFile(std::ofstream ofs) const override;
+    void saveToFile(std::ofstream& ofs) const override;
     void readFromFile(std::ifstream& ifs) override;
 private:
     const char* generateVerificationCode ();
 };
+
 

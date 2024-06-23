9fb31669b626fdb881163fce05500d36bca5a1d6
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 22:16:30 +0300

    Update Application.h

--- SystemManagement/Application.h
+++ SystemManagement/Application.h
@@ -3,7 +3,7 @@
 #include "Vector.h"
 #include "Client.h"
 #include "BankEmployee.h"
-//#include "ExternalCompanyEmployee.h"
+#include "ExternalEmployee.h"
 #include "Bank.h"
 
 enum class UserType {
@@ -37,20 +37,46 @@ public:
     void executeCommand(const MyString& command, const Vector<MyString>& args);
 
     void addMessageToClient(int clientId, const MyString& message);
+    Bank* findBank(const MyString& bankName);
+    const Bank* findBank(const MyString& bankName) const;
 
+    void signupExternalEmployee(const MyString& firstName, const MyString& surname,
+                                const MyString& EGN, unsigned age, const MyString& password);
+    ExternalEmployee* findExternalEmployeeById(int employeeId);
+
+
+    Client* findClientById(int clientId) ;
+    BankEmployee* getLeastBusyEmployee(const MyString& bankName);
+    const BankEmployee* getLeastBusyEmployee(const MyString& bankName) const;
+
+    bool validate(const MyString& taskId) const;
+
+    double checkAccountBalance (const MyString& bankName, const MyString& accountNumber, int clientId);
+
+    void list (const MyString& bankName, int clientId) ;
+
+    void listAccounts(const MyString& bankName, int clientId);
+    void sendCheck(double sum, const MyString& verificationCode, const MyString& egn);
+    void approveTask(const MyString& taskId, int employeeId);
+    void disapproveTask(const MyString& taskId, const MyString& message, int employeeId);
+    void validateTask(const MyString& taskId, int employeeId);
+
+    void redeemCheck(const MyString& bankName,
+                                  const MyString& accountNumber, const MyString& verificationCode,
+                                  int clientId);
 private:
     User* logged = nullptr;
     UserType type = UserType::DEFAULT;
 
     Vector<Client> clients;
     Vector<BankEmployee> employees;
-    //Vector<ExternalCompanyEmployee> companyEmployees;
+    Vector<ExternalEmployee> companyEmployees;
     Vector<Bank> banks;
 
     Application() = default;
     Application(const Application& other) = delete;
     Application& operator=(const Application& other) = delete;
 
-    Bank* findBank(const MyString& bankName);
-    Client* findClientById(int clientId);
+
 };
+

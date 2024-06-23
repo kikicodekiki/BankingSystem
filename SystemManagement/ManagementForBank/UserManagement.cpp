fbef0fa11f159db61336e65d6850d450f2b0242b
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 10:46:43 +0300

    Update UserManagement.cpp
    fix dynamic cast issues

--- SystemManagement/ManagementForBank/UserManagement.cpp
+++ SystemManagement/ManagementForBank/UserManagement.cpp
@@ -1,8 +1,17 @@
 #include "UserManagement.h"
 
 void UserManagement::addUser(Polymorphic_Ptr<User> user) {
-    users.pushBack(std::move(user));
+    if(dynamic_cast<Client*>(user.get())) {
+        users.pushBack(std::move(user));
+        clientsCount++;
+    }
+
+    else if(dynamic_cast<BankEmployee*>(user.get())) {
+        users.pushBack(std::move(user));
+        employeesCount++;
+    }
 }
+
 
 Polymorphic_Ptr<User> UserManagement::findUser(const MyString& egn) const {
     int index = findUserIndex(egn);
@@ -29,4 +38,40 @@ int UserManagement::findUserIndex(const MyString& egn) const {
         }
     }
     return -1;
+}
+
+Polymorphic_Ptr<User> UserManagement::findUserByEgnArray ( const size_t egn[EGN_SIZE] )const {
+    for (size_t i = 0; i < users.getSize(); ++i) {
+        const size_t* userEgn = users[i]->getEgn();
+        bool match = true;
+        for (size_t j = 0; j < EGN_SIZE; ++j) {
+            if (userEgn[j] != egn[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            return users[i];
+        }
+    }
+    return nullptr;
+}
+
+BankEmployee* UserManagement::findLeastBusyEmployee() {
+    BankEmployee* leastBusyEmployee = nullptr;
+
+    for (size_t i = 0; i < users.getSize(); i++) {
+        BankEmployee* employee = dynamic_cast<BankEmployee*>(users[i].get());
+        if (employee) {
+            if (!leastBusyEmployee || employee->getTaskCount() < leastBusyEmployee->getTaskCount()) {
+                leastBusyEmployee = employee;
+            }
+        }
+    }
+    return leastBusyEmployee;
+}
+
+const BankEmployee* UserManagement::findLeastBusyEmployee() const {
+    
+    return const_cast<UserManagement*>(this)->findLeastBusyEmployee();
 }

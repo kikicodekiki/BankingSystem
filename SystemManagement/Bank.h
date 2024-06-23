dd6fa074c110310b338c2b3c1f861b9874107d53
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 19:47:30 +0300

    Update Bank.h

--- SystemManagement/Bank.h
+++ SystemManagement/Bank.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "AccountManagement.h"
+#pragma once
+#include "AccountManagement.h"
 #include "UserManagement.h"
 #include "Messaging.h"
 #include "MyString.h"
-
-
+#include "Task.h"
+#include "BankEmployee.h"
+#include "BankCheck.h"
 
 class Bank {
 public:
@@ -15,8 +18,10 @@ public:
     const MyString& getName() const;
 
     // User Management
-    void addUser(Polymorphic_Ptr<User> user);
+    void addUser(User* user);
     Polymorphic_Ptr<User> findUser(const MyString& egn) const;
+    Polymorphic_Ptr<User> findUser (const size_t arr[EGN_SIZE]) const;
+
 
     // Account Management
     bool createAccount(const MyString& accountNumber, double initialBalance, const MyString& ownerEgn);
@@ -28,6 +33,16 @@ public:
     void sendMessage(const MyString& userEgn, const MyString& message);
     Vector<MyString> getMessages(const MyString& userEgn) const;
 
+    BankEmployee* getLeastBusyEmployee();
+    const BankEmployee* getLeastBusyEmployee() const;
+
+    bool validateClientData (const Client* client, const MyString& accountNumber) const;
+
+    void issueCheck(const MyString& EGN, const BankCheck& check);
+    double redeemCheck (const MyString& accountNumber, const MyString& verificationCode);
+
+    void listAccounts (int clientId) ;
+
 private:
 
     MyString bankName;
@@ -35,4 +50,6 @@ private:
     AccountManagement accountManager;
     UserManagement userManager;
     Messaging messaging;
+
+    Vector<BankCheck> issuedChecks;
 };

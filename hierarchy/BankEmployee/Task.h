a145e98537819adc87d744a6be4d267481a3fe06
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 19:50:25 +0300

    Update Task.h

--- hierarchy/BankEmployee/Task.h
+++ hierarchy/BankEmployee/Task.h
@@ -1,22 +1,36 @@
 #pragma once
+#include "User.h"
+#include "MyString.h"
+#include "Vector.h"
+#include "TaskState.h"
+#include "Client.h"
+#include "Bank.h"
+
 enum class TaskType {
     OPEN,
     CLOSE,
     CHANGE,
-
     Default
 };
 
-struct Task {
+class Task {
+public:
     TaskType type = TaskType::Default;
     const Client* client = nullptr;
-    MyString details;
+    const Bank* bank = nullptr;
     unsigned taskId = 0;
+    TaskState* state = nullptr;
+    MyString accountNumber;
+    MyString details;
 
-    Task () = default;
-    Task (const TaskType& t, const Client* c, const MyString& d, unsigned id ) :
-            type(t), client(c), details(d), taskId(id) {}
-    ~Task() {
-        delete client;
-    }
+    Task() = default;
+    Task(const TaskType& t, const Client* c, const MyString& accNum, const MyString& d, unsigned id);
+    ~Task();
+
+    void approve();
+    void reject(const MyString& message);
+    void setState(TaskState* newState);
+
+private:
+    void cleanupState();
 };

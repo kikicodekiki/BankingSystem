a8c44fbf96744f00a9c13f132934101f0a0e0987
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 19:49:45 +0300

    Update BankEmployee.h

--- hierarchy/BankEmployee/BankEmployee.h
+++ hierarchy/BankEmployee/BankEmployee.h
@@ -1,20 +1,34 @@
 #pragma once
 #include "IBankEmployee.h"
 #include "Task.h"
+#include "Bank.h"
 
 class BankEmployee : public User, public virtual IBankEmployee {
 public:
-    BankEmployee(const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age, const MyString& password);
+    BankEmployee(const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age,
+                 const MyString& password, Bank* bank);
 
     void show_tasks() const override;
     void view(const MyString& taskId) const override;
     void approve(const MyString& taskId) override;
     void disapprove(const MyString& taskId, const MyString& message) override;
-    void validate(const MyString& taskId) override;
+    bool validate(const MyString& taskId) const override;
 
     void addTask(const Task& task);
     size_t getTaskCount() const;
 
+    Bank* getCurrentBank () const {return bank;}
+
+    ~BankEmployee() = default;
+
+
+    void help() const override;
+
+    User* clone() const override {
+        return new BankEmployee(*this);
+    }
+
 private:
     Vector<Task> tasks;
+    Bank* bank = nullptr; //does not own the bank object
 };

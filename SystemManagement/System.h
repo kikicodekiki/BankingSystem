9526765b794f23ce48324a4e69211e53ee2306e9
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-23 20:14:20 +0300

    Update System.h

--- SystemManagement/System.h
+++ SystemManagement/System.h
@@ -1,27 +1,43 @@
 #pragma once
+#include "CommandFactory.h"
 #include "Application.h"
-#include "CheckBalannceCommand.h"
-#include "OpenAccountCommand.h"
-#include "CloseAccountCommand.h"
-#include "ListAccountCommands.h"
+
 class System {
 public:
-    static void executeCommand (const MyString& command, const Vector<MyString> args) {
-
+    static void executeCommand(const MyString& command, const Vector<MyString>& args) {
         Application& app = Application::getInstance();
+        ICommand* cmd = nullptr;
 
-        if (command == "check_avl" && args.getSize() == 2) {
-            CheckBalanceCommand checkBalanceCmd(args[0], args[1], app.getLoggedUser()->getId());
-            checkBalanceCmd.execute();
-        } else if (command == "open" && args.getSize() == 1) {
-            OpenAccountCommand openAccountCmd(args[0], app.getLoggedUser()->getId());
-            openAccountCmd.execute();
-        } else if (command == "close" && args.getSize() == 2) {
-            CloseAccountCommand closeAccountCmd(args[0], args[1], app.getLoggedUser()->getId());
-            closeAccountCmd.execute();
-        } else if (command == "list" && args.getSize() == 1) {
-            ListAccountCommand listAccountsCmd(args[0], app.getLoggedUser()->getId());
-            listAccountsCmd.execute();
+        if (app.getLoggedType() == UserType::Client) {
+            if (command == "check_avl" && args.getSize() == 2) {
+                cmd = CommandFactory::createCheckBalanceCommand(args[0], args[1], app.getLoggedUser()->getId());
+            } else if (command == "open" && args.getSize() == 1) {
+                cmd = CommandFactory::createOpenAccountCommand(args[0], app.getLoggedUser()->getId());
+            } else if (command == "close" && args.getSize() == 2) {
+                cmd = CommandFactory::createCloseAccountCommand(args[0], args[1], app.getLoggedUser()->getId());
+            } else if (command == "change" && args.getSize() == 3) {
+                cmd = CommandFactory::createChangeAccountBankCommand(args[0], args[1], args[2], app.getLoggedUser()->getId());
+            } else if (command == "redeem" && args.getSize() == 3) {
+                cmd = CommandFactory::createRedeemCheckCommand(args[0], args[1], args[2], app.getLoggedUser()->getId());
+            }
+        } else if (app.getLoggedType() == UserType::Employee) {
+            if (command == "approve" && args.getSize() == 1) {
+                cmd = CommandFactory::createApproveTaskCommand(args[0], app.getLoggedUser()->getId());
+            } else if (command == "disapprove" && args.getSize() == 2) {
+                cmd = CommandFactory::createDisapproveTaskCommand(args[0], args[1], app.getLoggedUser()->getId());
+            } else if (command == "validate" && args.getSize() == 1) {
+                cmd = CommandFactory::createValidateTaskCommand(args[0], app.getLoggedUser()->getId());
+            }
+        } else if (app.getLoggedType() == UserType::ExternalEmployee) {
+            if (command == "send_check" && args.getSize() == 3) {
+                double sum = std::stod(args[0].c_str());
+                cmd = CommandFactory::createSendCheckCommand(sum, args[1], args[2], app.getLoggedUser()->getId());
+            }
+        }
+
+        if (cmd) {
+            cmd->execute();
+            delete cmd;
         } else {
             std::cout << "Unknown command or incorrect arguments.\n";
         }

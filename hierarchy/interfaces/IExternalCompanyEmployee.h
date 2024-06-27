65454806172fc0d9bd06acfc094caa9de11ad72c
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-27 17:47:21 +0300

    Delete hierarchy/interfaces directory

--- hierarchy/interfaces/IExternalCompanyEmployee.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "MyString.h"
-class IExternalEmployee {
-public :
-    virtual void send_check (double sum, const MyString& bankName, const MyString& EGN) = 0;
-    virtual ~IExternalEmployee() = 0;
-};

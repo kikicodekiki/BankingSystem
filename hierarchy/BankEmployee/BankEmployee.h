543b0e2699a7575ff9099643a716073b52fc2c21
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-27 19:23:28 +0300

    Update BankEmployee.h
    fix linker error

--- hierarchy/BankEmployee/BankEmployee.h
+++ hierarchy/BankEmployee/BankEmployee.h
@@ -1,18 +1,21 @@
 #pragma once
-#include "IBankEmployee.h"
+
 #include "Task.h"
 #include "Bank.h"
 
-class BankEmployee : public User, public virtual IBankEmployee {
+class Bank;
+
+class BankEmployee : public User {
 public:
+    BankEmployee()= default;
     BankEmployee(const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age,
                  const MyString& password, Bank* bank);
 
-    void show_tasks() const override;
-    void view(const MyString& taskId) const override;
-    void approve(const MyString& taskId) override;
-    void disapprove(const MyString& taskId, const MyString& message) override;
-    bool validate(const MyString& taskId) const override;
+    void show_tasks() const ;
+    void view(const MyString& taskId) const ;
+    void approve(const MyString& taskId) ;
+    void disapprove(const MyString& taskId, const MyString& message) ;
+    bool validate(const MyString& taskId) const ;
 
     void addTask(const Task& task);
     size_t getTaskCount() const;
@@ -21,10 +24,14 @@ public:
 
     ~BankEmployee() = default;
 
+    Vector<Task> getTasks ()  {
+        return tasks;
+    }
+    const Vector<Task>& getTasks() const { return tasks; }
 
-    void help() const override;
+    void help() const;
 
-    User* clone() const override {
+    User* clone() const override{
         return new BankEmployee(*this);
     }
 

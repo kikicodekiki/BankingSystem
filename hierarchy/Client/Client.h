0573c495cd3b03b0343b208614df42aea9d471fc
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-27 19:25:27 +0300

    Update Client.h
    fix linker errors - still logic errors though🫠

--- hierarchy/Client/Client.h
+++ hierarchy/Client/Client.h
@@ -1,39 +1,42 @@
 #pragma once
 #include "User.h"
-#include "IClient.h"
+
 #include "Vector.h"
 #include "MyString.h"
 #include <fstream>
 
-class Client : public User, public virtual IClient {
+class Client : public User {
 public:
+    Client() = default;
     Client(const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age,
            const MyString& password, const MyString& address);
     Client(MyString&& firstName, MyString&& surname, MyString&& EGN, unsigned age,
            MyString&& password, MyString&& address);
 
-    void check_avl(const MyString& bankName, const MyString& accountNumber) const override;
-    void open(const MyString& bankName) override;
-    void close(const MyString& bankName, const MyString& accountNumber) override;
-    void redeem(const MyString& bankName, const MyString& accountNumber, const MyString& verificationCode) override;
-    void change(const MyString& newBankName, const MyString& currBank, const MyString& accNum) override;
-    void list(const MyString& bankName) const override;
-    void messages() const override;
+    void check_avl(const MyString& bankName, const MyString& accountNumber) const ;
+    void open(const MyString& bankName) ;
+    void close(const MyString& bankName, const MyString& accountNumber) ;
+    void redeem(const MyString& bankName, const MyString& accountNumber, const MyString& verificationCode) ;
+    void change(const MyString& newBankName, const MyString& currBank, const MyString& accNum) ;
+    void list(const MyString& bankName) const ;
+    void messages() const;
 
     void addAccount(const MyString& accountName);
     void addMessage(const MyString& message);
 
-    void saveToFile(std::ofstream& ofs) const override;
-    void readFromFile(std::ifstream& ifs) override;
+//    void saveToFile(std::ofstream& ofs) const ;
+//    void readFromFile(std::ifstream& ifs) ;
 
-    void help() const override;
+    void help() const ;
 
     User* clone() const override {
         return new Client(*this);
     }
+
     Vector<MyString> getAccounts() const;
 private:
     MyString address;
-    Vector<MyString> bankAccounts;
+    //tuk trqbva da e vektor ot bank accounts
     Vector<MyString> messageInbox;
 };
+

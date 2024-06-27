b2504562e7fec91d15d77f7f7bf871bff0f654b5
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-27 19:40:07 +0300

    Update TaskState.h
    fix linker error

--- SystemManagement/TaskState.h
+++ SystemManagement/TaskState.h
@@ -1,17 +1,13 @@
 #pragma once
-#include "Task.h"
+#include "MyString.h"
+
+class Task;
 
 class TaskState {
 public:
     virtual void approve(Task& task) = 0;
     virtual void reject(Task& task, const MyString& message) = 0;
     virtual ~TaskState() = default;
-};
-
-class NeedsApproval : public TaskState {
-public:
-    void approve(Task& task) override;
-    void reject(Task& task, const MyString& message) override;
 };
 
 class Approved : public TaskState {
@@ -21,6 +17,12 @@ public:
 };
 
 class Rejected : public TaskState {
+public:
+    void approve(Task& task) override;
+    void reject(Task& task, const MyString& message) override;
+};
+
+class NeedsApproval : public TaskState {
 public:
     void approve(Task& task) override;
     void reject(Task& task, const MyString& message) override;

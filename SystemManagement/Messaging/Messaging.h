23d3655f2a0ef90233d6d9e78710c94df811a489
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-27 19:43:04 +0300

    Update Messaging.h
    fix linker

--- SystemManagement/Messaging/Messaging.h
+++ SystemManagement/Messaging/Messaging.h
@@ -1,16 +1,19 @@
 #pragma once
 #include "MyString.h"
 #include "Vector.h"
-#include "IMessaging.h"
 
-class Messaging : public IMessaging {
+
+class Messaging{
 public:
-    void sendMessage(const MyString& userEgn, const MyString& message) override;
-    Vector<MyString> getMessages(const MyString& userEgn) const override;
+    void sendMessage(const MyString& userEgn, const MyString& message) ;
+    Vector<MyString> getMessages(const MyString& userEgn) const;
 
 private:
     Vector<MyString> userEgns;
-    Vector<Vector<MyString>> userMessages;
+    Vector<Vector<MyString> > userMessages;
+
+
 
     int findUserMessagesIndex(const MyString& userEgn) const;
 };
+

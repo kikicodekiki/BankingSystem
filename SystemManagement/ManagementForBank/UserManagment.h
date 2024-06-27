f7cb06e2a2d3ee6a07239c766f6863ca73f85e04
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-27 19:41:20 +0300

    Update UserManagment.h
    fix linker

--- SystemManagement/ManagementForBank/UserManagment.h
+++ SystemManagement/ManagementForBank/UserManagment.h
@@ -1,23 +1,37 @@
 #pragma once
-#include "IUserManagment.h"
+//#include "IUserManagment.h"
 #include "PolymorphicPtr.h"
 #include "User.h"
 #include "Vector.h"
 #include "MyString.h"
 #include "Client.h"
-#include "BankEmployee.h"
-class UserManagement : public IUserManagement {
-public:
-    void addUser(Polymorphic_Ptr<User> user) override;
+//#include "BankEmployee.h"
+#include <fstream>
 
-    Polymorphic_Ptr<User> findUser(const MyString& egn) const override;
-    Polymorphic_Ptr<User> findUserByEgnArray ( const size_t egn[EGN_SIZE] )const override;
+class BankEmployee;
+
+class UserManagement {
+public:
+    void addUser(Polymorphic_Ptr<User> user) ;
+
+    Polymorphic_Ptr<User> findUser(const MyString& egn) const ;
+    Polymorphic_Ptr<User> findUserByEgnArray ( const size_t egn[EGN_SIZE] )const ;
+    Client* findClientById (int clientId) ;
+    User* findUserByEgn (const MyString& egn);
 
     BankEmployee* findLeastBusyEmployee();
     const BankEmployee* findLeastBusyEmployee() const;
 
+    Vector<Polymorphic_Ptr<User> > getUsers() const {
+        return users;
+    }
+
+//    void saveToFile (std::ofstream& ofs) const;
+//    void readFromFile (std::ifstream& ifs);
+
+
 private:
-    Vector<Polymorphic_Ptr<User>> users;
+    Vector<Polymorphic_Ptr<User> > users;
 
     int clientsCount = 0;
     int employeesCount = 0;

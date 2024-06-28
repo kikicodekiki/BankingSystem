dc4eab9c95e5efed03638e90960af333a53da931
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-28 15:13:31 +0300

    Update Client.h
    final version with all needed methods

--- hierarchy/Client/Client.h
+++ hierarchy/Client/Client.h
@@ -1,42 +1,32 @@
 #pragma once
 #include "User.h"
-
-#include "Vector.h"
-#include "MyString.h"
-#include <fstream>
+#include "Messaging.h"
+#include "BankCheck.h"
 
 class Client : public User {
 public:
     Client() = default;
-    Client(const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age,
-           const MyString& password, const MyString& address);
-    Client(MyString&& firstName, MyString&& surname, MyString&& EGN, unsigned age,
-           MyString&& password, MyString&& address);
+    Client(const MyString& firstName, const MyString& surname, const MyString& EGN, unsigned age, const MyString& password, const MyString& address);
 
-    void check_avl(const MyString& bankName, const MyString& accountNumber) const ;
-    void open(const MyString& bankName) ;
-    void close(const MyString& bankName, const MyString& accountNumber) ;
-    void redeem(const MyString& bankName, const MyString& accountNumber, const MyString& verificationCode) ;
-    void change(const MyString& newBankName, const MyString& currBank, const MyString& accNum) ;
-    void list(const MyString& bankName) const ;
-    void messages() const;
-
-    void addAccount(const MyString& accountName);
+    double getCheckSum(const MyString& verificationCode) const;
+    void addCheck(const BankCheck& check);
     void addMessage(const MyString& message);
+    void showMessages() const;
 
-//    void saveToFile(std::ofstream& ofs) const ;
-//    void readFromFile(std::ifstream& ifs) ;
+    void saveToFile(std::ofstream& ofs) const override;
+    void readFromFile(std::ifstream& ifs) override;
+    void help() const override;
+    User* clone() const override;
 
-    void help() const ;
-
-    User* clone() const override {
-        return new Client(*this);
+    MyString getFullName() const {
+        return name + " " + surname;
     }
 
-    Vector<MyString> getAccounts() const;
+
+
 private:
     MyString address;
-    //tuk trqbva da e vektor ot bank accounts
-    Vector<MyString> messageInbox;
+    Vector<BankCheck> checks;
+    Messaging messages;
 };
 

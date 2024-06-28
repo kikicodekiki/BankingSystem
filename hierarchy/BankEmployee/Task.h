6905f6dd088f56e392610dd48bb559c331996349
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-28 15:17:19 +0300

    Update Task.h
    add validation functionality

--- hierarchy/BankEmployee/Task.h
+++ hierarchy/BankEmployee/Task.h
@@ -1,37 +1,43 @@
 #pragma once
-#include "User.h"
 #include "MyString.h"
 #include "Vector.h"
 #include "TaskState.h"
-#include "Client.h"
+#include <fstream>
+#include "TaskType.h"
 
-#include "Bank.h"
 
-enum class TaskType {
-    OPEN,
-    CLOSE,
-    CHANGE,
-    Default
-};
+class Application;
+
 
 class Task {
-
 public:
     TaskType type = TaskType::Default;
-    const Client* client = nullptr;
-    Bank * bank = nullptr;
+    MyString clientName ;
+    MyString bankName;
     unsigned taskId = 0;
     TaskState* state = nullptr;
     MyString accountNumber;
     MyString details;
 
     Task() = default;
-    Task(const TaskType& t, const Client* c, const MyString& accNum, const MyString& d, unsigned id);
+    Task(const TaskType& t, const MyString& clientName, const MyString& bankName,
+         const MyString& accNum, const MyString& d, unsigned id);
     ~Task();
+
+    Task (const Task& other) = default;
+
+    void saveToFile (std::ofstream& ofs ) const;
+    void readFromFile (std::ifstream& ifs);
 
     void approve();
     void reject(const MyString& message);
     void setState(TaskState* newState);
+
+    void viewTask () const;
+
+    int getId () const {return taskId;}
+
+    void validate();
 
 private:
     void cleanupState();

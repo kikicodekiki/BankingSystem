94f76aa6ac62a7e7f7007c286455f06874c18b3a
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-28 15:14:44 +0300

    Update ExternalCompanyEmployee.h

--- hierarchy/ExternalEmployee/ExternalCompanyEmployee.h
+++ hierarchy/ExternalEmployee/ExternalCompanyEmployee.h
@@ -1,31 +1,19 @@
 #pragma once
 #include "User.h"
-#include "MyString.h"
-#include <random>
 
-
-
-class ExternalEmployee : public User {
+class ExternalEmployee: public User {
 public:
     ExternalEmployee() = default;
     ExternalEmployee(const MyString& firstName, const MyString& lastName,
-                            const MyString& EGN, size_t age, const MyString& password);
+                     const MyString& EGN, size_t age, const MyString& password);
     ExternalEmployee(MyString&& firstName, MyString&& lastName,
-            MyString& EGN, size_t age, MyString&& password);
-
-    void send_check (double sum, const char* verificationCode, const MyString& EGN) ;
-    void send_check (double sum, char*&& verificationCode, MyString&& EGN);
-
+                     MyString& EGN, size_t age, MyString&& password);
     void help() const;
 
     User* clone() const override {
         return new ExternalEmployee(*this);
     }
-protected:
     void saveToFile(std::ofstream& ofs) const ;
-    void readFromFile(std::ifstream& ifs) ;
-private:
-    const char* generateVerificationCode ();
+    void readFromFile(std::ifstream& ifs);
 };
-
 

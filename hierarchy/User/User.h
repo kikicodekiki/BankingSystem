546ab9973399be3cc6e2f059054bc825c91c7f37
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-06-28 15:12:08 +0300

    Update User.h
    add methods to read the file

--- hierarchy/User/User.h
+++ hierarchy/User/User.h
@@ -1,50 +1,39 @@
 #pragma once
 #include "MyString.h"
-
 #include <fstream>
 
 const size_t EGN_SIZE = 10;
 
-class User {
-public :
-    User() = default;
-    User (const MyString& name, const MyString& surname,
-          const MyString& EGN, size_t age,
-          const MyString& password);
-    User (MyString&& name, MyString&& surname,
-          MyString&& EGN, size_t age,
-          MyString&& password);
 
+class User {
+public:
+    User() = default;
+    User(const MyString& name, const MyString& surname, const MyString& EGN, size_t age, const MyString& password);
     virtual ~User() = default;
 
-    void whoAmI() const ;
+    virtual void whoAmI() const;
+    bool authenticate(const MyString& username, const MyString& password) const;
+    MyString getFullName () const;
 
-    bool authenticate (const MyString& username, const MyString& password) const;
+    virtual void saveToFile(std::ofstream& ofs) const;
+    virtual void readFromFile(std::ifstream& ifs);
+    virtual User* clone() const = 0;
 
-//    void saveToFile(std::ofstream& ofs) const ;
-//    void readFromFile(std::ifstream& ifs);
+    virtual void help() const = 0;
 
-    virtual User* clone () const = 0;
-
-    void help() const ;
+    int getId ()const {return id;}
 
     const size_t* getEgn() const {return EGN;}
-    MyString getFullName () const;
-    int getId () const {return id;}
 
 protected:
-
     MyString name;
     MyString surname;
     size_t EGN[EGN_SIZE];
     size_t age = 1;
     MyString password;
-
+    int id = 0;
     static int currId;
 
-    int id;
-
-    void setEGN (const MyString& EGN);
+    void setEGN(const MyString& EGN);
     void setPassword(const MyString& password);
-
 };
